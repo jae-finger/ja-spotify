ac33c5a6a67648e81bbd700a6de352ca789cc80c
Jon Finger <jaefinger@gmail.com>
2020-07-26 23:26:08 -0500

    creating api_routes.py

---
+++ web_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from web_app.routes.api_routes import api_routes
+
+
+app = Flask(__name__)
+
+
+def create_app():
+    """Instantiate Flask API."""
+    app = Flask(__name__)
+
+    app.register_blueprint(api_routes)
+
+    return app
+
+if __name__ == '__main__':
+    my_app = create_app()
+    my_app.run(debug=True)
